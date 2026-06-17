02371101de3ef448470afa29abd646fd461f94e3
cluster-cat-mods <casjava@outlook.com>
2026-06-17 15:50:10 +0200

    Improved pathing debugging

--- ProjectNewHorizons/Assets/Scripts/Enemy.cs
+++ ProjectNewHorizons/Assets/Scripts/Enemy.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using NaughtyAttributes;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -12,6 +10,11 @@ public class Enemy : MonoBehaviour
     private PathFinder _pathFinder;
     private Graph<Transform> _graph;
     private List<Transform> _path;
+    
+    [SerializeField] private bool drawDebug = false;
+    [SerializeField, ShowIf("drawDebug")] private Color pathColor = Color.red;
+    [SerializeField, ShowIf("drawDebug")] private Color spawnNodeColor = Color.green;
+    [SerializeField, ShowIf("drawDebug")] private Color endNodeColor = Color.red;
     
     [Button]
     private void SetStuff()
@@ -46,9 +49,14 @@ public class Enemy : MonoBehaviour
     {
         for (int i = 0; i < _path.Count - 1; i++)
         {
-            Debug.DrawLine(_path[i].position, _path[i + 1].position, Color.red);
-            Gizmos.color = Color.red;
+            Debug.DrawLine(_path[i].position, _path[i + 1].position, pathColor);
+            Gizmos.color = pathColor;
             Gizmos.DrawWireSphere((_path[i].position + _path[i + 1].position) / 2, 0.1f);
+            Gizmos.DrawWireSphere(_path[i].position, 0.1f);
         }
+        Gizmos.color = spawnNodeColor;
+        Gizmos.DrawWireSphere(spawnPoint.transform.position, 0.1f);
+        Gizmos.color = endNodeColor;
+        Gizmos.DrawWireSphere(endPoint.transform.position, 0.1f);
     }
 }
