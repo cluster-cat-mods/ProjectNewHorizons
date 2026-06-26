f33c7efc688daba00c953046c629cb140eae2470
jjnwaterpolo <jaronieuwenhuisen@gmail.com>
2026-06-26 11:07:58 +0200

    data saving

--- ProjectNewHorizons/Assets/Scripts/GameDataSaver.cs
+++ ProjectNewHorizons/Assets/Scripts/GameDataSaver.cs
@@ -2,19 +2,46 @@ using System.IO;
 using Unity.Collections;
 using UnityEngine;
 
-public class GameDataSaver : MonoBehaviour
+public class GameDataSaver
 {
-    private string path = Application.persistentDataPath + "/gameData.json";
+    private string path;
+    private string json;
     
     public void SaveGameData(int  corpseCount, StageReached[] maxStageReached)
     {
         GameData gameData = new GameData(corpseCount, maxStageReached);
+        json = JsonUtility.ToJson(gameData);
+
+#if (UNITY_WEBGL && !UNITY_EDITOR)
+        path = System.IO.Path.Combine("idbfs", Application.productName);
+        if (!File.Exists(path)) 
+        { 
+            Directory.CreateDirectory(path);
+        }
+        path = System.IO.Path.Combine(path, "saveAntData");
+#else
+        path = Application.persistentDataPath + "/gameData.json";
+#endif
         File.WriteAllText(path, JsonUtility.ToJson(JsonUtility.ToJson(gameData)));
     }
 
     public GameData LoadGameData()
     {
-        return JsonUtility.FromJson<GameData>(File.ReadAllText(path));
+#if (UNITY_WEBGL && !UNITY_EDITOR)
+        path = System.IO.Path.Combine("idbfs", Application.productName);
+        path = System.IO.Path.Combine(path, "saveAntData");
+#else
+        path = Application.persistentDataPath + "/gameData.json";
+#endif
+        if (File.Exists(path))
+        {
+            json = File.ReadAllText(path);
+            return JsonUtility.FromJson<GameData>(json);
+        }
+        else
+        {
+            return null;
+        }
     }
 }
 
