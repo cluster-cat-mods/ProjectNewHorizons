5dcafe7516d0cd6ac6592c56327945ffbf2c2f16
jjnwaterpolo <jaronieuwenhuisen@gmail.com>
2026-06-30 23:50:45 +0200

    try to make it so just before building the game reset data

---
+++ ProjectNewHorizons/Assets/Editor/JustBeforeBuild.cs
@@ -0,0 +1,16 @@
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class JustBeforeBuiild : IPreprocessBuildWithReport
+{
+    private DevFunctions dev = new();
+    public int callbackOrder => 0;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        Debug.Log("Running BEFORE build starts!");
+
+        dev.ResetFullSaveFile();
+    }
+}
