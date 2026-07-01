56168b614ec5680af4c07167aa1f22d75a49c299
jjnwaterpolo <jaronieuwenhuisen@gmail.com>
2026-07-01 15:46:59 +0200

    deleted non used functions in alive enemy manager

--- ProjectNewHorizons/Assets/Scripts/AliveEnemyManager.cs
+++ ProjectNewHorizons/Assets/Scripts/AliveEnemyManager.cs
@@ -5,18 +5,6 @@ public class AliveEnemyManager : MonoBehaviour
 {
     private List<GameObject> _aliveEnemyList = new();
     private List<Transform> _aliveEnemyTransformList = new();
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public List<GameObject> AliveEnemies()
     {
         return _aliveEnemyList;
